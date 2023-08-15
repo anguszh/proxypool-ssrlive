14fcc9a39f4d76934cca85dcc813ef740cb0dc50
anguszh <42165317+anguszh@users.noreply.github.com>
2023-08-15 21:32:44 +0800

    Delete http directory

--- http/config/source.go
+++
@@ -1,8 +0,0 @@
-package config
-
-import "github.com/ssrlive/proxypool/pkg/tool"
-
-type Source struct {
-	Type    string       `json:"type" yaml:"type"`
-	Options tool.Options `json:"options" yaml:"options"`
-}
